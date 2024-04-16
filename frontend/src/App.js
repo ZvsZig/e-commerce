0b8b5205bafb63d5286eb30e3ee0e90d35bbed06
Zig <zvszig@gmail.com>
2024-04-15 20:40:55 -0700

    fixed routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import Shop from './pages/Shop';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
 
 function App() {
   return (
@@ -9,7 +13,15 @@ function App() {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path = '/' element = {<shop/>}/>
+        <Route path = '/' element = {<Shop/>}/>
+        <Route path = '/mens' element = {<ShopCategory category='mens'/>}/>
+        <Route path = '/womens' element = {<shopCategory category='womens'/>}/>
+        <Route path = '/kids' element = {<shopCatergory category='kids'/>}/>
+        <Route path = '/product' element = {<Product/>}>
+          <Route path = ':productId' element = {<Product/>}/>
+        </Route>
+        <Route path = '/cart' element = {<Cart/>}/>
+        <Route path = '/login' element = {<LoginSignup/>}/>
       </Routes>
       </BrowserRouter>
     </div>
