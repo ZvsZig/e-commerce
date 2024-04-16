9529f6e50b781da018c9275a0463c10d1dc56009
Zig <zvszig@gmail.com>
2024-04-15 19:40:27 -0700

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path = '/' element = {<shop/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
