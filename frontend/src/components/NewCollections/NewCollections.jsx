5c6f2fc63f342769132cc7dd46a4c6dad4f2dff9
Zig <zvszig@gmail.com>
2024-04-17 16:21:58 -0700

    added and fixed collections css

--- frontend/src/components/NewCollections/NewCollections.jsx
+++ frontend/src/components/NewCollections/NewCollections.jsx
@@ -5,7 +5,7 @@ import Item from '../Item/Item'
 
 const NewCollections = () => {
   return (
-    <div classNam = 'new-collections'>
+    <div className = 'new-collections'>
         <h1>NEW COLLECTIONS</h1>
         <hr/>
         <div className = 'collections'>
