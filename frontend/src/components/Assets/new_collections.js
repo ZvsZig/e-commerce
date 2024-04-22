3559607ebfff16cf20a6c540697dddc8a5374101
Zig <zvszig@gmail.com>
2024-04-21 23:16:47 -0700

    altered asset files

--- frontend/src/components/Assets/new_collections.js
+++ frontend/src/components/Assets/new_collections.js
@@ -2,8 +2,8 @@ import p1_img from "./product_12.png";
 import p2_img from "./product_35.png";
 import p3_img from "./product_14.png";
 import p4_img from "./product_8.png";
-import p5_img from "./product_15.png";
-import p6_img from "./product_2.png";
+import p5_img from "./product_27.png";
+import p6_img from "./product_21.png";
 import p7_img from "./product_17.png";
 import p8_img from "./product_28.png";
 
@@ -17,7 +17,7 @@ let new_collections = [
   },
   {
     id: 35,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Facetime Camera for Zooming and Recording",
     image: p2_img,
     new_price: 85.0,
     old_price: 120.5,
@@ -37,15 +37,15 @@ let new_collections = [
     old_price: 150.0,
   },
   {
-    id: 15,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    id: 27,
+    name: "Streamer Microphone for Streaming",
     image: p5_img,
-    new_price: 50.0,
-    old_price: 80.5,
+    new_price: 85.0,
+    old_price: 120.5,
   },
   {
-    id: 2,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    id: 21,
+    name: "Hole-y Mouse for Gaming",
     image: p6_img,
     new_price: 85.0,
     old_price: 120.5,
@@ -59,7 +59,7 @@ let new_collections = [
   },
   {
     id: 28,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Bluetooth Memory Saver",
     image: p8_img,
     new_price: 100.0,
     old_price: 150.0,
