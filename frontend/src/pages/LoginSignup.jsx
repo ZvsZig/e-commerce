b0f72f063905bd9b84d909a532b0e85cc10c64a5
Zig <zvszig@gmail.com>
2024-04-23 17:08:09 -0700

    login page created

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './CSS/loginsignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-        
+    <div className='login-signup'>
+        <div className='loginsignup-container'>
+          <h1>Sign Up</h1>
+          <div className='loginsignup-fields'>
+            <input type='text' placeholder='Your Name'/>
+            <input type='email' placeholder='Email Address'/>
+            <input type='password' placeholder='Password'/>
+          </div>
+          <button>Continue</button>
+          <p className='loginsignup-login'>Already have an account? <span>Log In Here</span></p>
+          <div className='loginsignup-agree'>
+            <input type='checkbox' name = '' id = ''/>
+            <p>
+              By continuing, I agree to the terms of use and privacy policy.
+            </p>
+          </div>
+        </div>
     </div>
   )
 }
