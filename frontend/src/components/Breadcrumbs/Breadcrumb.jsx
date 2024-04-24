b77121a81b5c8d1af661462153f9f036cc07fc25
Zig <zvszig@gmail.com>
2024-04-23 17:21:40 -0700

    added breadcrumbs and product page

---
+++ frontend/src/components/Breadcrumbs/Breadcrumb.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Breadcrumb.css'
+import arrow_icon from '../Assets/breadcrumb_arrow.png'
+
+const Breadcrumb = (props) => {
+    const { product } = props;
+    return (
+        <div className='breadcrumb'>
+            HOME <img src={arrow_icon} alt='arrow' />
+            SHOP
+            <img src={arrow_icon} alt='arrow' />
+            {product.category}
+            <img src={arrow_icon} alt='arrow' />
+            {product.name}
+        </div>
+    )
+}
+
+export default Breadcrumb
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {useContext} from 'react'
+import {ShopContext} from '../context/ShopContext'
+import {useParams} from 'react-router-dom'
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb'
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams();
+  // ensures that id matches product before displaying on page
+  const product = all_product.find((e) => e.id === Number(productId)); 
   return (
     <div>
-        
+        <Breadcrumb product = {product}/>
     </div>
   )
 }
