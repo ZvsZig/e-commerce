b707e8e63893d649a1f5145b384fde72d8adcf4b
Zig <zvszig@gmail.com>
2024-04-23 21:33:03 -0700

    express server running

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,21 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+// Database connection with MongoDB
+mongoose.connect('mongodb+srv://zvszig:957pRpv*@cluster0.gyj9wln.mongodb.net/e-commerce');
+
+//api creation
+
+app.get('/', (req, res) => {
+    res.send('Expresss App is Running')
+})
+
+
+app.listen(port, (error) => {
+    if(!error) {
+        console.log('Server is up and running on port '+port);
+    }
+    else{
+        console.log('Error: '+error)
+    }
+})
