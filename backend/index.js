1c83bb34a939833b794eb42362bb34100a38ae04
Zig <zvszig@gmail.com>
2024-04-24 07:05:20 -0700

    added user registration model

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path')
 const cors = require('cors');
+const { type } = require('os');
 
 app.use(express.json());
 app.use(cors());
@@ -124,6 +125,58 @@ app.get('/allproducts', async(req, res) => {
     console.log("All Products Fetched");
     res.send(products);
 })
+
+//Schema for creating User model
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    cartData:{
+        type: Object,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    }
+})
+
+// Creating endpoint for user registration
+app.post('/signup', async(req, res) => {
+    let check = await Users.findOne({email: req.body.email});
+    if (check){
+        //prevents same email from signing up
+        return res.status(400).json({success:false, errors:"User already exists"})
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    })
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({success:true, token})
+})
+
+
 
 app.listen(port, (error) => {
     if(!error) {
