918730fc21f61fdff847b498388cebabcbb46dd2
Zig <zvszig@gmail.com>
2024-04-24 07:57:30 -0700

    logout button made

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -23,7 +23,9 @@ const Navbar = () => {
         <li onClick = {() => {setMenu("tech")}}><Link style={{ textDecoration: 'none'}} to = '/tech'>Tech</Link>{menu==="tech"?<hr/>:<></>}</li>
       </ul>
       <div className='nav-login-cart'>
-        <Link to = '/login'><button>Login</button></Link>
+        {localStorage.getItem('auth-token')
+        ?<button onClick={() => {localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+      :<Link to = '/login'><button>Login</button></Link>}
         <Link to = '/cart'><img src={cart_icon} alt='cart'/></Link>
         <div className='nav-cart-count'>{getTotalCartItems()}</div>
       </div>
