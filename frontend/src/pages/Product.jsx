cfdd6f9c3f84d96b93b0f63019332b838328446e
Zig <zvszig@gmail.com>
2024-04-23 18:47:42 -0700

    added related products functionality

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -5,6 +5,7 @@ import {useParams} from 'react-router-dom'
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb'
 import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
 import DescriptionBox from '../components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -16,6 +17,7 @@ const Product = () => {
         <Breadcrumb product = {product}/>
         <ProductDisplay product = {product}/>
         <DescriptionBox/>
+        <RelatedProducts/>
     </div>
   )
 }
