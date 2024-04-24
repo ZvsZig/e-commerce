c4fabcb701378d307a0097cf6745055326603621
Zig <zvszig@gmail.com>
2024-04-23 17:43:21 -0700

    created base product display

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -3,6 +3,7 @@ import {useContext} from 'react'
 import {ShopContext} from '../context/ShopContext'
 import {useParams} from 'react-router-dom'
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb'
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -12,6 +13,7 @@ const Product = () => {
   return (
     <div>
         <Breadcrumb product = {product}/>
+        <ProductDisplay product = {product}/>
     </div>
   )
 }
